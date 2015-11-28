5dc4988a14f6ec817adb6d8d4b2aab6ecc28c8af
MarinAtanasov <marin.atanasov.bg@gmail.com>
2015-11-28 21:03:04 +0200

    Removed unused usings.

--- AppBrix.Backgammon.Core/Game/IDiceRoller.cs
+++ AppBrix.Backgammon.Core/Game/IDiceRoller.cs
@@ -2,9 +2,7 @@
 // Licensed under the MIT License (MIT). See License.txt in the project root for license information.
 //
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace AppBrix.Backgammon.Core.Game
 {
