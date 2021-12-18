1250fd2b6360afa6d853da553a10b59f97ee4905
MarinAtanasov <marin.atanasov.bg@gmail.com>
2021-12-18 18:36:38 +0200

    Upgrade AppBrix to 0.27.0

--- Modules/AppBrix.Backgammon/Events/IGameEvent.cs
+++ Modules/AppBrix.Backgammon/Events/IGameEvent.cs
@@ -2,7 +2,7 @@
 // Licensed under the MIT License (MIT). See License.txt in the project root for license information.
 //
 using AppBrix.Backgammon.Game;
-using AppBrix.Events;
+using AppBrix.Events.Contracts;
 
 namespace AppBrix.Backgammon.Events;
 
